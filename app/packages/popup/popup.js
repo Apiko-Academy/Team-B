aa8196cdf5ff81f46c7c17de2153c47c60cdcb57
Stepan Lopushansky <stepan.te.ua@gmail.com>
2015-08-10 14:25:16 +0300

    popup position

--- app/packages/popup/popup.js
+++ app/packages/popup/popup.js
@@ -1,9 +1,20 @@
 Template.PopupToggle.events({
     'click >': function(e) {
         e.preventDefault();
+        var globalTpl = Template.instance();
+        var el = globalTpl.$('>');
         var thisDataId = Template.currentData().id;
-        $('[data-popup=' + thisDataId + ']').toggle('slow');
+        console.log(thisDataId);
+        var popover = $('[data-popup=' + thisDataId + ']');
 
-        //var top = thisDataId.position.top;
+        var top = el.position().top - 70;
+        var left = el.position().left;
+        left = left - (popover.width() / 2) + (el.width() / 2) - 2;
+
+
+        popover.css('opacity', 1).css('left', left).css('top', top).toggle('slow');
+        popover.find('input').focus();
+        //$('[data-popup=' + thisDataId + ']').toggle('slow');
+
     }
 });
